0134f75b481fdc3e4af4d1c0d8f3b607541ce340
Sangram Bishwas <info.sangram2003@gmail.com>
2024-05-13 03:41:52 +0600

    make another get operation for dynamic id of posts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -39,6 +39,14 @@ async function run() {
             const cursor = postCollections.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/posts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await postCollections.findOne(query);
+            res.send(result);
+
         })
 
         await client.db("admin").command({ ping: 1 });
