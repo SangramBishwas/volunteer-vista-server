2631cbe515598295861882607202f8bf0c10d4bf
Sangram Bishwas <info.sangram2003@gmail.com>
2024-05-13 23:20:43 +0600

    delete  operation has been done for posts

--- index.js
+++ index.js
@@ -57,7 +57,15 @@ async function run() {
             const result = await postCollections.find(query).toArray();
             res.send(result);
         })
-        
+
+        app.delete('/posts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await postCollections.deleteOne(query);
+            res.send(result);
+
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
